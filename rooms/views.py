406da12e5b4cbd4f2f53bf7a326d7295e0fd788c
Lee Hyun Jun <mikey1000@naver.com>
2022-03-24 18:00:07 +0900

    Listview with CBV & FBV

--- rooms/views.py
+++ rooms/views.py
@@ -1,3 +1,33 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.generics import ListAPIView
+from .models import Room
+from .serializers import RoomSerializer
 
 # Create your views here.
+
+'''
+# FBV
+
+@api_view(["GET"])
+def list_rooms(request):
+    rooms = Room.objects.all()
+    serialized_rooms = RoomSerializer(rooms, many=True)
+    return Response(data=serialized_rooms.data)
+'''
+
+'''
+# CBV(APIView)
+class ListRoomsView(APIView):
+    
+    def get(self,request):
+        rooms = Room.objects.all()
+        serializer = RoomSerializer(rooms, many=True)
+        return Response(serializer.data)
+'''
+
+class ListRoomsView(ListAPIView):
+    queryset = Room.objects.all()
+    serializer_class = RoomSerializer
