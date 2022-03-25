fa1ca7fdd1eddcf9691f54636a8a2e1cf21ca086
Lee Hyun Jun <mikey1000@naver.com>
2022-03-25 17:39:01 +0900

    nested serializers

--- rooms/serializers.py
+++ rooms/serializers.py
@@ -1,13 +1,11 @@
 from rest_framework import serializers
+from users.serializers import UserSerializer
 from .models import Room
 
-class RoomSerializer(serializers.Serializer):
-    name = serializers.CharField(max_length=140)
-    price = serializers.IntegerField()
-    bedrooms = serializers.IntegerField()
-    instant_book = serializers.BooleanField()
+class RoomSerializer(serializers.ModelSerializer):
     
-class BigRoomSerializer(serializers.ModelSerializer):
+    user = UserSerializer()
+    
     class Meta:
         model = Room
         fields = ("__all__")
