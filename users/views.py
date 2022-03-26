1252f0a67462f9d231fb74a765ec60c4915be13f
Lee Hyun Jun <mikey1000@naver.com>
2022-03-26 17:00:23 +0900

    id->pk

--- users/views.py
+++ users/views.py
@@ -1,3 +1,6 @@
+import jwt
+from django.conf import settings
+from django.contrib.auth import authenticate
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.decorators import api_view
@@ -7,6 +10,21 @@ from rooms.models import Room
 from rooms.serializers import RoomSerializer
 from .models import User
 from .serializers import UserSerializer
+
+@api_view(["POST"])
+def login(request):
+    username = request.data.get("username")
+    password = request.data.get("password")
+    if not username or not password:
+        return Response(status=status.HTTP_400_BAD_REQUEST)
+    user = authenticate(username=username, password=password)
+    if user is not None:
+        encoded_jwt = jwt.encode(
+            {"pk": user.pk}, settings.SECRET_KEY, algorithm="HS256"
+        )
+        return Response(data={"token": encoded_jwt})
+    else:
+        return Response(status=status.HTTP_401_UNAUTHORIZED)
 
 class UsersView(APIView):
     def post(self, request):
