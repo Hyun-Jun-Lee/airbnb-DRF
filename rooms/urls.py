d4a8f4825ebe976c58fa5ee7025491df7ea7ab31
Lee Hyun Jun <mikey1000@naver.com>
2022-03-29 01:21:20 +0900

    url router

--- rooms/urls.py
+++ rooms/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
+from rest_framework.routers import DefaultRouter
 from . import views
 
 app_name = "rooms"
 
-urlpatterns = [
-    path("", views.RoomsView.as_view()),
-    path("<int:pk>/", views.RoomView.as_view()),
-]
+router = DefaultRouter()
+router.register("", views.RoomViewSet)
+
+
+urlpatterns = router.urls
